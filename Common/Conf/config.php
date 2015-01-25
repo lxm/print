826cc392c359727c7ea7d9e3e8d33b398a538c8d
New-Future <new_future@msn.cn>
2015-01-25 15:17:25 +0800

    修改配置，系统配置和安全配置分离

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -8,8 +8,11 @@
 // | Copyright (c) 2014 云印南开团队 All rights reserved.
 // +------------------------------------------------------------------
 return array(
+    //安全配置文件，相同内容可以覆盖配置文件    
+    'LOAD_EXT_CONFIG' => 'secret',
+
     //调试信息
-	'SHOW_PAGE_TRACE' => 1,
+   'SHOW_PAGE_TRACE' => 1,
 
     //SQL生成缓存
     'DB_SQL_BUILD_CACHE' => true,
@@ -18,40 +21,38 @@ return array(
     'VERIFY_WAY'=>'Common.Urp',
 
     //URL设定
-    'URL_PARAMS_BIND'       =>  TRUE,
-    'URL_MODEL'=>2,
+    'URL_PARAMS_BIND'          =>    TRUE,
+    'URL_MODEL'                        =>    2,
     'MODULE_ALLOW_LIST'    =>    array('Home','Printer',),
-	'DEFAULT_MODULE'       =>    'Home',  // 默认模块
+    'DEFAULT_MODULE'           =>    'Home',  // 默认模块
 
     //数据库配置
-	'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'print',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '',          // 密码
-    'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  '',    // 数据库表前缀
+    'DB_TYPE'                    =>    'mysql',     // 数据库类型
+    'DB_PORT'                  =>    '3306',        // 端口
+    'DB_PREFIX'               =>    '',    // 数据库表前缀
    
     //模板渲染转义
     'TMPL_PARSE_STRING' => array(
-        '__PUBLIC__' => '/Public',
-        '__JS__' => '/Public/js',
-        '__CSS__' => '/Public/css',
-        '__UPLOAD__' => '/Uploads',
+        '__PUBLIC__'     =>   '/Public',
+        '__JS__'                =>   '/Public/js',
+        '__CSS__'            =>   '/Public/css',
+        '__UPLOAD__'   =>   '/Uploads',
     ) ,
 
     //用户登录类型
-	'STUDENT' => 1,
-	'PRINTER' => 2,
-    'PRINTER_WEB' => 3,
+    'STUDENT'            => 1,
+    'PRINTER'             => 2,
+    'PRINTER_WEB'  => 3,
+    'STUDENT_API'   => 4 ,
+
     //文件状态
-    'FILE_DELETED'=>0,
-    'FILE_UPLOAD'=>1,
-    'FILE_DOWNLOAD'=>2,
-    'FILE_PRINT'=>3,
-    'FILE_PRINTED'=>4,
-    'FILE_PAID'=>5,
+    'FILE_DELETED'         =>0,
+    'FILE_UPLOAD'          =>1,
+    'FILE_DOWNLOAD'  =>2,
+    'FILE_PRINT'              =>3,
+    'FILE_PRINTED'         =>4,
+    'FILE_PAID'                 =>5,
 
     //最大尝试次数
-    'MAX_TRIES'=>10,
+    'MAX_TRIES'             =>10,
 );
